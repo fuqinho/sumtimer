d9e1f6c170595a6c416dca836eda2784c8290741
Naoki Fukino <naoki.fukino@gmail.com>
2022-10-14 07:40:32 +0900

    Define some types for upcoming data collections.

--- src/components/models.ts
+++ src/components/models.ts
@@ -13,3 +13,42 @@ export interface CategoryDocumentData extends DocumentData {
   name: string;
   color: string;
 }
+
+export interface CategoryData {
+  id: number;
+  name: string;
+  color?: string;
+  order?: number;
+}
+
+export interface OngoingRecord {
+  activity_id?: string;
+  start: Date;
+  breaks: { start: Date; end?: Date }[];
+}
+
+export interface UserDocumentData extends DocumentData {
+  categories: CategoryData[];
+  ongoing?: OngoingRecord;
+}
+
+export interface ActivityDocumentData extends DocumentData {
+  uid: string;
+  title: string;
+  description?: string;
+  category_id?: string;
+  color?: string;
+  progress_max?: number;
+}
+
+export interface RecordDocumentData extends DocumentData {
+  uid: string;
+  activity_id?: string;
+  start: Date;
+  end: Date;
+  breaks: { start: Date; end: Date }[];
+  duration_seconds?: number;
+  whats_done?: string;
+  progress_from?: number;
+  progress_to?: number;
+}
