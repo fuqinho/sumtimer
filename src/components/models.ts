5185fb8a9108fa002076b1efd0abdc5a9f929462
Naoki Fukino <naoki.fukino@gmail.com>
2022-10-14 12:53:00 +0900

    Update data models for upcoming use.

--- src/components/models.ts
+++ src/components/models.ts
@@ -15,10 +15,9 @@ export interface CategoryDocumentData extends DocumentData {
 }
 
 export interface CategoryData {
-  id: number;
-  name: string;
+  id: string;
+  label: string;
   color?: string;
-  order?: number;
 }
 
 export interface OngoingRecord {
@@ -34,21 +33,43 @@ export interface UserDocumentData extends DocumentData {
 
 export interface ActivityDocumentData extends DocumentData {
   uid: string;
-  title: string;
-  description?: string;
-  category_id?: string;
+  label: string;
+  cid?: string;
   color?: string;
-  progress_max?: number;
+  desc?: string;
+  progress?: {
+    progress_max?: number;
+    progress_unit?: string;
+  };
+  cache?: {
+    progress_now?: number;
+    elapsed_time?: number;
+    recent_records?: {
+      start: Date;
+      end: Date;
+      duration: number;
+      whats_done?: string;
+      progress_from?: number;
+      progress_to?: number;
+    }[];
+  };
 }
 
 export interface RecordDocumentData extends DocumentData {
   uid: string;
-  activity_id?: string;
+  aid?: string;
   start: Date;
   end: Date;
   breaks: { start: Date; end: Date }[];
-  duration_seconds?: number;
+  duration?: number;
   whats_done?: string;
   progress_from?: number;
   progress_to?: number;
+}
+
+// This might be necesary to reduce document reads.
+export interface WeekCacheDocumentData extends DocumentData {
+  uid: string;
+  start: Date;
+  cache: RecordDocumentData[];
 }
