4787f0e678b885b090045684dcdf4b9d5aff7596
Naoki Fukino <naoki.fukino@gmail.com>
2022-10-19 11:51:01 +0900

    Make record's activity editable.

--- src/components/RecordForm.vue
+++ src/components/RecordForm.vue
@@ -2,9 +2,10 @@
 import { ref, computed } from 'vue';
 import { Timestamp } from '@firebase/firestore';
 import { RecordDocumentData } from 'src/common/types';
-import { useActivityStore } from 'src/stores/activity-store';
+import { ActivityData, useActivityStore } from 'src/stores/activity-store';
 import { useRecordStore } from 'src/stores/record-store';
 import DateTimeInput from 'src/components/DateTimeInput.vue';
+import { useUserDataStore } from 'src/stores/user-data-store';
 
 interface Props {
   record_id?: string;
@@ -13,6 +14,7 @@ interface Props {
 const props = defineProps<Props>();
 const emit = defineEmits(['onSaved']);
 
+const userStore = useUserDataStore();
 const recordStore = useRecordStore();
 const activityStore = useActivityStore();
 const startTime = ref(
@@ -46,8 +48,40 @@ async function updateRecord() {
     const newData = props.record_data;
     newData.start = Timestamp.fromDate(startTime.value);
     newData.end = Timestamp.fromDate(endTime.value);
+    newData.aid = selectedActivity.value
+      ? selectedActivity.value.aid
+      : undefined;
     await recordStore.updateRecord(props.record_id, newData);
     emit('onSaved');
+  }
+}
+
+function categoryName(activity: ActivityData) {
+  if (activity.data.cid) {
+    const data = userStore.getCategoryData(activity.data.cid);
+    if (data && data.label) {
+      return data.label;
+    }
+  }
+  return undefined;
+}
+
+const activityOptions = ref(
+  activityStore.activities.map((activity) => {
+    return {
+      aid: activity.id,
+      label: activity.data.label,
+      category: categoryName(activity),
+    };
+  })
+);
+const selectedActivity = ref(null as { aid: string; label: string } | null);
+if (props.record_data) {
+  for (const option of activityOptions.value) {
+    if (option.aid === props.record_data.aid) {
+      selectedActivity.value = option;
+      break;
+    }
   }
 }
 </script>
@@ -58,6 +92,24 @@ async function updateRecord() {
     <q-card-section>
       <date-time-input v-model:time="startTime" />
       <date-time-input v-model:time="endTime" />
+      <q-select
+        v-model="selectedActivity"
+        :options="activityOptions"
+        label="Activity"
+      >
+        <template v-slot:option="scope">
+          <q-item v-bind="scope.itemProps">
+            <q-item-section>
+              <q-item-label>
+                {{ scope.opt.label }}
+              </q-item-label>
+              <q-item-label caption>
+                {{ scope.opt.category }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
     </q-card-section>
     <q-card-actions align="right">
       <q-btn label="Cancel" flat v-close-popup></q-btn>
