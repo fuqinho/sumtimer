6f08af95c00b75be257f759f6cad45e479ea1d39
Naoki Fukino <naoki.fukino@gmail.com>
2022-10-22 21:06:05 +0900

    Make category editable from category tab.

--- src/pages/ActivitiesPage.vue
+++ src/pages/ActivitiesPage.vue
@@ -5,14 +5,31 @@ import CategoryTab from 'src/components/CategoryTab.vue';
 import { ref } from 'vue';
 import { useCategoryStore } from 'src/stores/category-store';
 import { storeToRefs } from 'pinia';
+import CategoryForm from 'src/components/CategoryForm.vue';
+import { CategoryDoc } from 'src/common/types';
 
 const categoryStore = useCategoryStore();
 
 const { categories } = storeToRefs(categoryStore);
 
-const creating = ref(false);
+const creatingCategory = ref(false);
+const creatingActivity = ref(false);
+const editingCategory = ref(false);
 const splitterModel = ref(20);
 const tab = ref('');
+const currentCategory = ref(undefined as CategoryDoc | undefined);
+
+function onEditCategoryClicked(cid: string) {
+  let category = undefined;
+  for (const c of categories.value) {
+    if (c.id === cid) {
+      category = c;
+      break;
+    }
+  }
+  currentCategory.value = category;
+  editingCategory.value = true;
+}
 </script>
 
 <template>
@@ -32,20 +49,47 @@ const tab = ref('');
               v-for="category in categories"
               :key="category.id"
               :doc="category"
+              @on-edit="onEditCategoryClicked"
             />
           </q-tabs>
+          <q-btn
+            @click="creatingCategory = true"
+            color="grey"
+            outline
+            rounded
+            icon="add"
+            label="Add category"
+            size="md"
+            style="margin: 12px 8px"
+          />
         </div>
       </template>
       <template v-slot:after>
         <ActivityList />
-        <q-btn class="q-ma-md" @click="creating = true"
-          >Add new activity.</q-btn
-        >
+        <q-btn
+          @click="creatingActivity = true"
+          color="grey"
+          outline
+          rounded
+          icon="add"
+          label="Add activity"
+          size="md"
+          style="margin: 0 16px"
+        />
       </template>
     </q-splitter>
   </div>
 
-  <q-dialog v-model="creating">
-    <ActivityForm @on-created="creating = false" />
+  <q-dialog v-model="creatingActivity">
+    <ActivityForm @on-created="creatingActivity = false" />
+  </q-dialog>
+  <q-dialog v-model="creatingCategory">
+    <CategoryForm @on-added="creatingCategory = false" />
+  </q-dialog>
+  <q-dialog v-model="editingCategory">
+    <CategoryForm
+      @on-updated="editingCategory = false"
+      :doc="currentCategory"
+    />
   </q-dialog>
 </template>
