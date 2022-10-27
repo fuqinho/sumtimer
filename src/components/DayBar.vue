5f6d2f2f6ae246d8059dcd5b19947aab5ce1f408
Naoki Fukino <naoki.fukino@gmail.com>
2022-10-27 19:31:26 +0900

    Show breaks in day bars.

--- src/components/DayBar.vue
+++ src/components/DayBar.vue
@@ -2,10 +2,7 @@
 import { computed, watch, onBeforeMount, onUnmounted, ref } from 'vue';
 import { date } from 'quasar';
 import { OngoingDocumentData, RecordDoc } from 'src/common/types';
-import { useActivityStore } from 'src/stores/activity-store';
-import { useCategoryStore } from 'src/stores/category-store';
-import { storeToRefs } from 'pinia';
-import { defaultCategoryColor } from 'src/common/constants';
+import RecordBar from 'src/components/RecordBar.vue';
 
 // =========================== Properties/Emitters =============================
 interface Props {
@@ -16,80 +13,34 @@ interface Props {
 const props = defineProps<Props>();
 
 // =========================== Use stores/composables ==========================
-const categoryStore = useCategoryStore();
-const activityStore = useActivityStore();
 const ongoing = computed(() => props.ongoing);
 
-// =========================== Computed properties =============================
-interface BarData {
-  rid?: string;
-  style: {
-    left: string;
-    width: string;
-    backgroundColor: string;
-    outlineColor?: string;
-    outlineWidth?: string;
-    outlineStyle?: string;
-  };
-}
+// =========================== Refs ============================================
+const now = ref(Date.now());
+const nowMarkerStyle = ref(null as { left: string } | null);
 
+// =========================== Computed properties =============================
 function hasIntersection(s0: number, e0: number, s1: number, e1: number) {
   return s0 < e1 && s1 < e0;
 }
 
-function barGeometry(
-  recStart: number,
-  recEnd: number,
-  dayStart: number,
-  dayEnd: number
-): [number, number] {
-  const startTime = Math.max(recStart, dayStart);
-  const endTime = Math.min(recEnd, dayEnd);
-  const startHour = (startTime - dayStart) / (60 * 60 * 1000);
-  const leftPercent = Math.round((startHour / 24) * 10000) / 100;
-  const durationHour = (endTime - startTime) / (60 * 60 * 1000);
-  const widthPercent = Math.round((durationHour / 24) * 10000) / 100;
-  return [leftPercent, widthPercent];
-}
-
-function barColor(aid?: string) {
-  if (aid) {
-    const activityData = idToActivity.value[aid];
-    if (activityData && activityData.cid) {
-      const categoryData = idToCategory.value[activityData.cid];
-      if (categoryData && categoryData.color) {
-        return categoryData.color;
-      }
-    }
-  }
-  return defaultCategoryColor;
-}
-
-const bars = computed(() => {
+const overlapRecords = computed(() => {
   const dayStart = props.start.getTime();
   const dayEnd = date.addToDate(props.start, { days: 1 }).getTime();
-  const res = [] as BarData[];
+  const res = [] as RecordDoc[];
   for (const record of props.records) {
     const recStart = record.data.start.toMillis();
     const recEnd = record.data.end.toMillis();
     if (hasIntersection(recStart, recEnd, dayStart, dayEnd)) {
-      const [left, width] = barGeometry(recStart, recEnd, dayStart, dayEnd);
-      const color = barColor(record.data.aid);
-      res.push({
-        rid: record.id,
-        style: {
-          left: left + '%',
-          width: width + '%',
-          backgroundColor: color,
-        },
-      });
+      res.push(record);
     }
   }
   return res;
 });
-
-const ongoingBar = ref(null as BarData | null);
-const nowMarkerStyle = ref(null as { left: string } | null);
+const dayStartMillis = computed(() => props.start.getTime());
+const dayEndMillis = computed(() =>
+  date.addToDate(props.start, { days: 1 }).getTime()
+);
 
 watch(ongoing, () => {
   updateBar();
@@ -98,7 +49,7 @@ watch(ongoing, () => {
 let timerId = 0;
 onBeforeMount(() => {
   updateBar();
-  timerId = window.setInterval(updateBar, 20000);
+  timerId = window.setInterval(updateBar, 10000);
 });
 
 onUnmounted(() => {
@@ -109,32 +60,10 @@ onUnmounted(() => {
 function updateBar() {
   const dayStart = props.start.getTime();
   const dayEnd = date.addToDate(props.start, { days: 1 }).getTime();
-  if (ongoing.value) {
-    const recStart = ongoing.value.recStart.toMillis();
-    const recEnd = Date.now();
-    if (hasIntersection(recStart, recEnd, dayStart, dayEnd)) {
-      const [left, width] = barGeometry(recStart, recEnd, dayStart, dayEnd);
-      const color = barColor(ongoing.value.aid);
-      ongoingBar.value = {
-        style: {
-          left: left + '%',
-          width: width + '%',
-          backgroundColor: color,
-          outlineColor: 'rgba(255, 255, 0, 0.8)',
-          outlineWidth: '2px',
-          outlineStyle: 'solid',
-        },
-      };
-    } else {
-      ongoingBar.value = null;
-    }
-  } else {
-    ongoingBar.value = null;
-  }
 
-  const now = Date.now();
-  if (now >= dayStart && now < dayEnd) {
-    const startHour = (now - dayStart) / (60 * 60 * 1000);
+  now.value = Date.now();
+  if (now.value >= dayStart && now.value < dayEnd) {
+    const startHour = (now.value - dayStart) / (60 * 60 * 1000);
     const leftPercent = Math.round((startHour / 24) * 10000) / 100;
     nowMarkerStyle.value = { left: leftPercent + '%' };
   } else {
@@ -152,10 +81,6 @@ const separatorStyles = computed(() => {
   return res;
 });
 
-// =========================== Refs ============================================
-const { idToCategory } = storeToRefs(categoryStore);
-const { idToActivity } = storeToRefs(activityStore);
-
 // =========================== Methods =========================================
 // =========================== Additional setup ================================
 </script>
@@ -170,13 +95,20 @@ const { idToActivity } = storeToRefs(activityStore);
       class="bar-separator"
       :style="sep"
     />
-    <div
-      v-for="bar in bars"
-      :key="bar.rid"
-      class="bar"
-      :style="bar.style"
-    ></div>
-    <div v-if="ongoingBar" class="bar" :style="ongoingBar.style"></div>
+    <RecordBar
+      v-for="record in overlapRecords"
+      :key="record.id"
+      :day-start="dayStartMillis"
+      :day-end="dayEndMillis"
+      :doc="record"
+    />
+    <RecordBar
+      v-if="props.ongoing"
+      :day-start="dayStartMillis"
+      :day-end="dayEndMillis"
+      :ongoing="props.ongoing"
+      :now="now"
+    />
     <q-icon
       v-if="nowMarkerStyle"
       color="orange"
@@ -195,17 +127,6 @@ const { idToActivity } = storeToRefs(activityStore);
   height: 16px;
   margin: 4px 0;
   position: relative;
-}
-
-.bar {
-  position: absolute;
-  top: 0;
-  height: 100%;
-}
-
-.bar:hover {
-  box-shadow: $shadow-2;
-  cursor: pointer;
 }
 
 .now-marker-icon {
---
+++ src/components/RecordBar.vue
@@ -0,0 +1,165 @@
+<script setup lang="ts">
+import { Timestamp } from '@firebase/firestore';
+import { storeToRefs } from 'pinia';
+import { colors } from 'quasar';
+import { defaultCategoryColor } from 'src/common/constants';
+import { OngoingDocumentData, RecordDoc } from 'src/common/types';
+import { useActivityStore } from 'src/stores/activity-store';
+import { useCategoryStore } from 'src/stores/category-store';
+import { computed } from 'vue';
+
+interface Props {
+  dayStart: number;
+  dayEnd: number;
+  doc?: RecordDoc;
+  ongoing?: OngoingDocumentData;
+  now?: number;
+}
+const props = defineProps<Props>();
+
+const categoryStore = useCategoryStore();
+const activityStore = useActivityStore();
+
+const { idToCategory } = storeToRefs(categoryStore);
+const { idToActivity } = storeToRefs(activityStore);
+
+const categoryColor = computed(() => {
+  const aid =
+    (props.doc && props.doc.data.aid) || (props.ongoing && props.ongoing.aid);
+  if (aid) {
+    const activity = idToActivity.value[aid];
+    if (activity) {
+      const cid = activity.cid;
+      if (cid) {
+        const category = idToCategory.value[cid];
+        if (category) {
+          return category.color;
+        }
+      }
+    }
+  }
+  return defaultCategoryColor;
+});
+
+const lightCategoryColor = computed(
+  //() => colors.lighten(categoryColor.value, 90)
+  () => colors.changeAlpha(categoryColor.value + 'ff', -0.88)
+);
+
+const containerStyle = computed(() => {
+  let recStart = 0;
+  let recEnd = 1;
+  if (props.doc) {
+    recStart = props.doc.data.start.toMillis();
+    recEnd = props.doc.data.end.toMillis();
+  } else if (props.ongoing) {
+    recStart = props.ongoing.recStart.toMillis();
+    recEnd = props.now || Date.now();
+  }
+  if (props.doc) {
+    recStart = props.doc.data.start.toMillis();
+    recEnd = props.doc.data.end.toMillis();
+  } else if (props.ongoing) {
+    recStart = props.ongoing.recStart.toMillis();
+    recEnd = props.now || Date.now();
+  }
+  const total = 24 * 60 * 60 * 1000;
+
+  const left = (recStart - props.dayStart) / total;
+  const width = (recEnd - recStart) / total;
+  if (props.ongoing) {
+    return {
+      left: left * 100 + '%',
+      width: width * 100 + '%',
+      backgroundColor: lightCategoryColor.value,
+      outlineColor: 'rgba(255, 255, 0, 0.8)',
+      outlineWidth: '2px',
+      outlineStyle: 'solid',
+    };
+  } else {
+    return {
+      left: left * 100 + '%',
+      width: width * 100 + '%',
+      backgroundColor: lightCategoryColor.value,
+    };
+  }
+});
+
+const subStyles = computed(() => {
+  let recStart = 0;
+  let recEnd = 1;
+  if (props.doc) {
+    recStart = props.doc.data.start.toMillis();
+    recEnd = props.doc.data.end.toMillis();
+  } else if (props.ongoing) {
+    recStart = props.ongoing.recStart.toMillis();
+    recEnd = props.now || Date.now();
+  }
+  const total = recEnd - recStart;
+  console.log(total);
+
+  return subs.value.map((sub) => {
+    const left = (sub.start.toMillis() - recStart) / total;
+    const width = (sub.end.toMillis() - sub.start.toMillis()) / total;
+    return {
+      left: left * 100 + '%',
+      width: width * 100 + '%',
+      backgroundColor: categoryColor.value,
+    };
+  });
+});
+
+const subs = computed(() => {
+  if (props.doc) {
+    if (props.doc.data.subs) return props.doc.data.subs;
+    else return [{ start: props.doc.data.start, end: props.doc.data.end }];
+  }
+  if (props.ongoing) {
+    let res = [] as { start: Timestamp; end: Timestamp }[];
+    if (props.ongoing.subs) res = res.concat(props.ongoing.subs);
+    if (props.ongoing.curStart) {
+      res.push({
+        start: props.ongoing.curStart,
+        end: props.now ? Timestamp.fromMillis(props.now) : Timestamp.now(),
+      });
+    }
+    return res;
+  }
+  return [];
+});
+</script>
+
+<template>
+  <div class="container" :style="containerStyle">
+    <div
+      class="sub"
+      v-for="(sub, i) in subs"
+      :key="sub.start.toMillis()"
+      :style="subStyles[i]"
+    ></div>
+
+    <div class="sub"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  top: 0;
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  background-color: yellow;
+  height: 100%;
+  cursor: pointer;
+}
+
+.container:hover {
+  box-shadow: $shadow-2;
+}
+
+.sub {
+  top: 0;
+  position: absolute;
+  height: 100%;
+}
+</style>
