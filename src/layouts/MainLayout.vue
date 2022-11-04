4172c8b5d9b9ab71c92f5aa372f41908fc63234d
Naoki Fukino <naoki.fukino@gmail.com>
2022-11-05 06:21:01 +0900

    Change default header color.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -23,7 +23,7 @@ function signOutUser() {
 
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header reveal bordered class="bg-primary text-white" height-hint="98">
+    <q-header reveal bordered class="bg-grey-7 text-white" height-hint="98">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
