ea597e2ec67031d8d092d9708255b7394154c8a9
Naoki Fukino <naoki.fukino@gmail.com>
2022-12-09 09:05:14 +0900

    Scroll to top on starting an activity.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) return savedPosition;
+    if (to.hash) return { selector: to.hash };
+    return { top: 0 };
+  },
   routes: [
     {
       path: '/',
