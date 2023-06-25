db78de80aa7c84aebf045aa0474f6abeea67a7e9
Naoki Fukino <naoki.fukino@gmail.com>
2023-06-25 13:31:36 +0900

    Increment app version.

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,4 @@
-export const appVersion = 4;
+export const appVersion = 5;
 export const startHourOfDay = 5;
 export const startDayOfWeek = 1; // Monday
 export const defaultCategoryColor = '#bdbdbd';
